4977971c4819f6cb8785209b39316392b92fbb3c
Sebastian0328 <alvarezpaezsebastian03@gmail.com>
2024-03-27 11:58:20 +0100

    primer trabajo

---
+++ src/app/pages/tea-detail/tea-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tea-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './tea-detail.component.html',
+  styleUrl: './tea-detail.component.scss'
+})
+export class TeaDetailComponent {
+
+}
---
+++ src/app/pages/tea-list/tea-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tea-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './tea-list.component.html',
+  styleUrl: './tea-list.component.scss'
+})
+export class TeaListComponent {
+
+}
