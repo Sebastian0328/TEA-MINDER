91e1634fba2ef6a9f51f0cb9c3bbe886afdc3249
Sebastian0328 <alvarezpaezsebastian03@gmail.com>
2024-04-08 14:37:12 +0200

    formularios

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,38 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgIf, ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  login:FormGroup
+  constructor( private form: FormBuilder){
+   this.login= this.form.group({
+     email:['',[Validators.required,Validators.email]],
+     password:['',[Validators.required, Validators.minLength(8)]]
+   })
+  }
+  hasErros(controlName:string, errorType: string
+  ){
+ return this.login.get(controlName)?.hasError(errorType)&& this.login.get(controlName)?.touched }
+ enviar(){
+   // Marcar todos los campos como tocados para que se muestren los errores al enviar
+   this.login.markAllAsTouched();
 
-}
+   // Si el formulario es válido, puedes proceder con el envío
+   if (this.login.valid) {
+     console.log('El formulario es válido. Realizando envío...');
+     console.log(this.login.value); // Aquí puedes enviar los datos a tu servidor o realizar otras acciones
+   } else {
+     console.log('El formulario no es válido. Revise los campos resaltados en rojo.');
+   }}
+
+
+ }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,12 +1,38 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule,NgIf],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  register:FormGroup
+ constructor( private form: FormBuilder){
+  this.register= this.form.group({
+    name:['',Validators.required],
+    email:['',[Validators.required,Validators.email]],
+    password:['',[Validators.required, Validators.minLength(8)]]
+  })
+ }
+ hasErros(controlName:string, errorType: string
+ ){
+return this.register.get(controlName)?.hasError(errorType)&& this.register.get(controlName)?.touched }
+enviar(){
+  this.register.markAllAsTouched();
+
+   // Si el formulario es válido, puedes proceder con el envío
+   if (this.register.valid) {
+     console.log('El formulario es válido. Realizando envío...');
+     console.log(this.register.value); // Aquí puedes enviar los datos a tu servidor o realizar otras acciones
+   } else {
+     console.log('El formulario no es válido. Revise los campos resaltados en rojo.');
+   }}
+
+
 
 }
+
